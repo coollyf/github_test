76a60421c843ce0f963a23e3f7d55b6b6af6a1bf
liyunfei <1273862524@qq.com>
2018-03-28 16:04:22 +0800

    test

---
+++ src/main/resources/java/test.java
@@ -0,0 +1,5 @@
+public public static void start(android.content.Context context) {
+    android.content.Intent starter = new Intent(context, .class);
+    starter.putExtra();
+    context.startActivity(starter);
+}
