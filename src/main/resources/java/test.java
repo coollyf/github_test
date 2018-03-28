b99ed53c037d5f67dede4bbe8bc4fa1debc86a22
coollyf <37857143+coollyf@users.noreply.github.com>
2018-03-28 16:42:11 +0800

    Delete test.java

--- src/main/resources/java/test.java
+++
@@ -1,5 +0,0 @@
-public public static void start(android.content.Context context) {
-    android.content.Intent starter = new Intent(context, .class);
-    starter.putExtra();
-    context.startActivity(starter);
-}
