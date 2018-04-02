7fc0e0ca246030b4217f442d22dbc1085961a68d
liyunfei <1273862524@qq.com>
2018-04-02 11:31:52 +0800

    数据库链接

---
+++ src/main/java/com/myProject/dao/AuthUserMapper.java
@@ -0,0 +1,30 @@
+package com.myProject.dao;
+
+import com.myProject.dao.AuthUser;
+import com.myProject.dao.AuthUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface AuthUserMapper {
+    int countByExample(AuthUserExample example);
+
+    int deleteByExample(AuthUserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(AuthUser record);
+
+    int insertSelective(AuthUser record);
+
+    List<AuthUser> selectByExample(AuthUserExample example);
+
+    AuthUser selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") AuthUser record, @Param("example") AuthUserExample example);
+
+    int updateByExample(@Param("record") AuthUser record, @Param("example") AuthUserExample example);
+
+    int updateByPrimaryKeySelective(AuthUser record);
+
+    int updateByPrimaryKey(AuthUser record);
+}
